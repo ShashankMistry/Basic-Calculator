1fb8957187300f9173e8fe748d4daedd0c75a756
Shashank Mistry <53445530+ShashankMistry@users.noreply.github.com>
2021-01-06 13:42:06 +0530

    Update HistoryAdapter.java

--- app/src/main/java/com/shashank/calculator/HistoryAdapter.java
+++ app/src/main/java/com/shashank/calculator/HistoryAdapter.java
@@ -1,68 +1,38 @@
 package com.shashank.calculator;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-
-import androidx.lifecycle.ViewModelProviders;
-
-
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
 import java.util.Objects;
 
+public class HistoryAdapter extends ArrayAdapter<HistoryList> {
+    private final Context mContext;
 
-public class HistoryFragment extends Fragment {
-    ArrayList<HistoryList> historyLists;
-    SharedViewModel viewModel;
-    String History;
-    ListView listView;
-    Button btnClear;
-    View rootView;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_history, container, false);
-        btnClear = rootView.findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(v -> {
-            historyLists.clear();
-            listView.setAdapter(null);
-        });
-        historyLists = new ArrayList<>();
-        return rootView;
+    public HistoryAdapter(@NonNull Context context, @NonNull List<HistoryList> objects) {
+        super(context, 0, objects);
+        this.mContext = context;
     }
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        // getting data from SharedViewModel and setting it to ListView
-        viewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(SharedViewModel.class);
-        viewModel.getText().observe(getViewLifecycleOwner(), charSequence -> {
-            History = String.valueOf(charSequence);
-            String[] HistoryArray = History.split("=");
-            String[] RemoveDot = HistoryArray[1].split("\\.");
-            if (RemoveDot.length > 1) {
-                if (RemoveDot[1].equals("0")) {
-                    HistoryArray[1] = RemoveDot[0];
-                }
-            }
-            if (History.split("=").length == 2) {
-                historyLists.add(new HistoryList(HistoryArray[0], HistoryArray[1]));
-                HistoryAdapter adapter = new HistoryAdapter(Objects.requireNonNull(getActivity()), historyLists);
-                Collections.reverse(historyLists);
-                listView = rootView.findViewById(R.id.history);
-                listView.setAdapter(adapter);
-            }
-        });
+    public View getView(int position, @Nullable View convertView, ViewGroup parent) {
+        String Calc = Objects.requireNonNull(getItem(position)).getCalculation();
+        String Ans = Objects.requireNonNull(getItem(position)).getAnswer();
+        if(convertView==null) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(R.layout.history_list, parent, false);
+        }
+         TextView tvCalc = convertView.findViewById(R.id.calc);
+         TextView tvAns = convertView.findViewById(R.id.answer);
+         tvCalc.setText(Calc);
+         tvAns.setText(Ans);
+        return convertView;
     }
 }

ccded85ca0930baa9bc949a4ea6d0c8641223cfd
Shashank Mistry <53445530+ShashankMistry@users.noreply.github.com>
2021-01-09 12:46:42 +0530

    Update HistoryFragment.java

--- app/src/main/java/com/shashank/calculator/HistoryFragment.java
+++ app/src/main/java/com/shashank/calculator/HistoryFragment.java
@@ -1,16 +1,21 @@
 package com.shashank.calculator;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -31,9 +36,20 @@ public class HistoryFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_history, container, false);
         btnClear = rootView.findViewById(R.id.btnClear);
         listView = rootView.findViewById(R.id.history);
+        Vibrator vibe = (Vibrator) Objects.requireNonNull(getActivity()).getSystemService(Context.VIBRATOR_SERVICE);
         btnClear.setOnClickListener(v -> {
             historyLists.clear();
             listView.setAdapter(null);
+        });
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            HistoryList historyList = historyLists.get(position);
+            String ans = historyList.getAnswer();
+            vibe.vibrate(10);
+            ClipboardManager clipboard = (ClipboardManager) Objects.requireNonNull(getActivity()).getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("CalcData", ans);
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(getContext(), "Copied to clipboard", Toast.LENGTH_SHORT).show();
+            return false;
         });
         historyLists = new ArrayList<>();
         return rootView;
